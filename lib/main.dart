3e80979c9c51c8241b03e36a655497e71ffe6005
Nabin Chand <nbc0117rdx@gmail.com>
2023-01-12 17:38:04 +0545

    'awesome_notify

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,30 @@
+import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:firebase_notify/modal/notify.dart';
 import 'package:firebase_notify/welcome.dart';
 import 'package:flutter/material.dart';
 
-Future<void> _messageHandler(RemoteMessage message) async {
-  print('background message ${message.notification!.body}');
-}
+// Future<void> _messageHandler(RemoteMessage message) async {
+//   print('background message ${message.notification!.body}');
+// }
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  FirebaseMessaging.onBackgroundMessage(_messageHandler);
+void main() {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp();
+  // FirebaseMessaging.onBackgroundMessage(_messageHandler);
 
   runApp(const MyApp());
+  AwesomeNotifications().initialize(null, [
+    NotificationChannel(
+      channelGroupKey: 'remainder',
+      channelKey: 'intant_notification',
+      channelName: 'Basic Instant Notification',
+      channelDescription: 'Are you gettting hungry! Dont worry.',
+      defaultColor: const Color(0xFF9D50DD),
+      ledColor: Colors.white,
+    ),
+  ]);
 }
 
 class MyApp extends StatefulWidget {
@@ -23,45 +35,41 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  get initializationSettings => null;
-
-  get onSelectNotification => null;
-
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
 
-    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-      print("message recieved");
-      print(message.notification!.body);
-      print(message.data.values);
-      // showDialog(
-      //     context: context,
-      //     builder: (BuildContext context) {
-      //       return AlertDialog(
-      //         title: Text("Notification"),
-      //         content: Text(event.notification!.body!),
-      //         actions: [
-      //           TextButton(
-      //             child: Text("Ok"),
-      //             onPressed: () {
-      //               Navigator.of(context).pop();
-      //             },
-      //           )
-      //         ],
-      //       );
-      //     });
-    });
-    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
-      print('Message clicked!');
-      Navigator.push(
-        context,
-        MaterialPageRoute<void>(builder: (context) => const Welcome()),
-      );
-      // }
-      //
-    });
+    // FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    //   print("message recieved");
+    //   print(message.notification!.body);
+    //   print(message.data.values);
+    // showDialog(
+    //     context: context,
+    //     builder: (BuildContext context) {
+    //       return AlertDialog(
+    //         title: Text("Notification"),
+    //         content: Text(event.notification!.body!),
+    //         actions: [
+    //           TextButton(
+    //             child: Text("Ok"),
+    //             onPressed: () {
+    //               Navigator.of(context).pop();
+    //             },
+    //           )
+    //         ],
+    //       );
+    //     });
+    // });
+    // FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+    //   print('Message clicked!');
+    //   Navigator.push(
+    //     context,
+    //     MaterialPageRoute<void>(builder: (context) => const Welcome()),
+    //   );
+    // }
+    //
+    // });
   }
 
   Widget build(BuildContext context) {
@@ -73,7 +81,16 @@ class _MyAppState extends State<MyApp> {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(appBar: AppBar(), body: Text('hello')),
+      home: Scaffold(
+          appBar: AppBar(),
+          body: Center(
+            child: TextButton(
+              onPressed: () {
+                Notify.instantNotify();
+              },
+              child: const Text("Instant notification"),
+            ),
+          )),
       //
     );
   }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,7 @@
 import FlutterMacOS
 import Foundation
 
+import awesome_notifications
 import firebase_analytics
 import firebase_auth
 import firebase_core
@@ -12,6 +13,7 @@ import firebase_messaging
 import flutter_local_notifications
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AwesomeNotificationsPlugin.register(with: registry.registrar(forPlugin: "AwesomeNotificationsPlugin"))
   FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
